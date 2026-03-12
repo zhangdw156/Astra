25c68d9408fb2254b34a3db87d53c349cc5045c5
zhangdw <zhangdw.cs@gmail.com>
2026-03-12 16:29:12 +0800

    chore: update src/astra/utils

--- src/astra/utils/__init__.py
+++ src/astra/utils/__init__.py
@@ -1,7 +1,7 @@
 """Astra 工具：日志、UI、配置等。"""
 
-from astra.utils import config
-from astra.utils.logging import setup_logging
-from astra.utils.ui import print_trace, welcome_dashboard
+from . import config
+from .logging import setup_logging, logger
+from .ui import print_trace, welcome_dashboard
 
-__all__ = ["config", "setup_logging", "print_trace", "welcome_dashboard"]
+__all__ = ["config", "setup_logging", "logger", "print_trace", "welcome_dashboard"]
