d242cb4aa0b0bf542d0ab5dd17100d2eb80fb954
zhangdw <zhangdw.cs@gmail.com>
2026-03-12 19:02:09 +0800

    feat: update src/astra/agent/__init__.py

--- src/astra/agent/__init__.py
+++ src/astra/agent/__init__.py
@@ -1,9 +1,12 @@
 from ._planner_agent import PlannerAgent, PlannerAgentConfig
 from ._skill_agent import SkillAgent, SkillAgentConfig
+from ._tool_agent import ToolAgent, ToolAgentConfig
 
 __all__ = [
     "PlannerAgent",
     "PlannerAgentConfig",
     "SkillAgent",
     "SkillAgentConfig",
+    "ToolAgent",
+    "ToolAgentConfig",
 ]
